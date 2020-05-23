c5e19ccfa710ea07acf1b115da628cecc3db1399
lwl <lwl@xkool.xyz>
2020-05-23 16:57:08 +0800

    update readme

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 It's a shapely extension package for shapely user.
 shapely_ext provides lots of magic util bedded on shapely, including interpolate, mesh, space explore,
  angle and decomposition
+ 
+ 
+## install
+`pip install shapely_ext`
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="a shapely extension package providing magic function",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/pyeprog/shapely_ext",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
