cb3bc32b16dd76ea4a31c6f453a2681c31afaa7c
lwl <lwl@xkool.xyz>
2020-05-24 13:04:38 +0800

    refactor decompose

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="shapely_ext",
-    version="0.1.1",
+    version="0.1.2",
     author="pd",
     author_email="pyeprog@foxmail.com",
     description="a shapely extension package providing magic function",
--- shapely_ext/decompose.py
+++ shapely_ext/decompose.py
@@ -1,43 +1,125 @@
+from itertools import chain
 from typing import List
 
-from shapely.geometry import Polygon, LineString
+from shapely.geometry import Polygon, LineString, LinearRing, MultiPolygon, MultiLineString, GeometryCollection
 from shapely.geometry.base import BaseGeometry
 
 from shapely_ext.geometry.vector_2d import Vector2D
 
 
-def _is_corner(cur_coord, prev_coord, next_coord, min_corner_angle_degree: float) -> bool:
-    prev_vec = Vector2D.from_coordinates(prev_coord, cur_coord)
-    next_vec = Vector2D.from_coordinates(cur_coord, next_coord)
-    angle_in_degree = prev_vec.angle_to(next_vec)
-    return angle_in_degree >= min_corner_angle_degree
+class Decompose:
+    @staticmethod
+    def decompose_to_line(
+            geometry: BaseGeometry,
+            min_corner_angle_degree: float = 0,
+            simplify_radius: float = 1e-6) -> List[BaseGeometry]:
+        geometry = geometry.simplify(simplify_radius)
+        if isinstance(geometry, Polygon):
+            return Decompose.decompose_polygon_to_line(
+                polygon=geometry,
+                min_corner_angle_degree=min_corner_angle_degree)
+        elif isinstance(geometry, MultiPolygon):
+            lines_list: List[List[LineString]] = [
+                Decompose.decompose_polygon_to_line(
+                    polygon=sub_polygon,
+                    min_corner_angle_degree=min_corner_angle_degree)
+                for sub_polygon in list(geometry)]
+            return list(chain.from_iterable(lines_list))
+        elif isinstance(geometry, LinearRing):
+            return Decompose.decompose_linearRing_to_line(
+                ring=geometry,
+                min_corner_angle_degree=min_corner_angle_degree)
+        elif isinstance(geometry, LineString):
+            return Decompose.decompose_lineString_to_line(
+                lineString=geometry,
+                min_corner_angle_degree=min_corner_angle_degree)
+        elif isinstance(geometry, MultiLineString):
+            lines_list: List[List[LineString]] = [
+                Decompose.decompose_lineString_to_line(
+                    lineString=sub_lineString,
+                    min_corner_angle_degree=min_corner_angle_degree)
+                for sub_lineString in list(geometry)]
+            return list(chain.from_iterable(lines_list))
+        elif isinstance(geometry, GeometryCollection):
+            lines_list: List[List[LineString]] =[
+                Decompose.decompose_to_line(
+                    geometry=sub_geometry,
+                    min_corner_angle_degree=min_corner_angle_degree)
+                for sub_geometry in list(geometry)]
+            return list(chain.from_iterable(lines_list))
+        else:
+            return []
 
+    @staticmethod
+    def decompose_lineString_to_line(lineString: LineString, min_corner_angle_degree: float):
+        coords = list(lineString.coords)
+        lines: List[LineString] = []
+        cur_line_coords = []
+        for i in range(len(coords)):
+            cur_line_coords.append(coords[i])
+            if i - 1 < 0 or i + 1 >= len(coords):
+                continue
 
-def decompose(
-        geometry: BaseGeometry,
-        min_corner_angle_degree: float = 0,
-        simplify_radius: float = 1e-6) -> List[BaseGeometry]:
-    geometry = geometry.simplify(simplify_radius)
-    if isinstance(geometry, Polygon):
-        exterior_coords = list(geometry.exterior.coords)[:-1]
+            if Decompose._is_corner(coords[i], coords[i - 1], coords[i + 1],
+                                    min_corner_angle_degree=min_corner_angle_degree):
+                lines.append(LineString(cur_line_coords))
+                cur_line_coords = [coords[i]]
+        if cur_line_coords:
+            lines.append(LineString(cur_line_coords))
+        return lines
+
+    @staticmethod
+    def decompose_polygon_to_line(
+            polygon: Polygon, min_corner_angle_degree: float) -> List[LineString]:
+        exterior_lines: List[LineString] = Decompose.decompose_linearRing_to_line(
+            ring=polygon.exterior,
+            min_corner_angle_degree=min_corner_angle_degree)
+        interior_lines_list: List[List[LineString]] = [
+            Decompose.decompose_linearRing_to_line(
+                ring=interior,
+                min_corner_angle_degree=min_corner_angle_degree)
+            for interior in polygon.interiors
+        ]
+        return exterior_lines + list(chain.from_iterable(interior_lines_list))
+
+    @staticmethod
+    def decompose_linearRing_to_line(
+            ring: LinearRing,
+            min_corner_angle_degree: float = 0) -> List[LineString]:
+        coords = list(ring.coords)[:-1]
+        corner_i = Decompose._find_first_corner_index(coords, min_corner_angle_degree)
+        lines: List[LineString] = []
+        cur_line = [coords[corner_i]]
+        for j in range(1, len(coords) + 1):
+            cur_i = (corner_i + j) % len(coords)
+            cur_line.append(coords[cur_i])
+            if Decompose._is_corner(coords[cur_i], coords[cur_i - 1],
+                                    coords[(cur_i + 1) % len(coords)],
+                                    min_corner_angle_degree=min_corner_angle_degree):
+                lines.append(LineString(cur_line))
+                cur_line = [coords[cur_i]]
+        return lines
+
+    @staticmethod
+    def _find_first_corner_index(coords, min_corner_angle_degree):
         corner_i = None
-        for i in range(len(exterior_coords)):
-            last_last_coord = exterior_coords[i - 2]
-            last_coord = exterior_coords[i - 1]
-            cur_coord = exterior_coords[i]
-            if _is_corner(last_coord, last_last_coord, cur_coord, min_corner_angle_degree=min_corner_angle_degree):
+        for i in range(len(coords)):
+            last_last_coord = coords[i - 2]
+            last_coord = coords[i - 1]
+            cur_coord = coords[i]
+            if Decompose._is_corner(last_coord,
+                                    last_last_coord,
+                                    cur_coord,
+                                    min_corner_angle_degree=min_corner_angle_degree):
                 corner_i = i - 1
                 break
         if corner_i is None:
             raise ValueError("Polygon has no corner")
-        lines: List[LineString] = []
-        cur_line = [exterior_coords[corner_i]]
-        for j in range(1, len(exterior_coords) + 1):
-            cur_i = (corner_i + j) % len(exterior_coords)
-            cur_line.append(exterior_coords[cur_i])
-            if _is_corner(exterior_coords[cur_i], exterior_coords[cur_i - 1],
-                          exterior_coords[(cur_i + 1) % len(exterior_coords)],
-                          min_corner_angle_degree=min_corner_angle_degree):
-                lines.append(LineString(cur_line))
-                cur_line = [exterior_coords[cur_i]]
-        return lines
+        return corner_i
+
+    @staticmethod
+    def _is_corner(cur_coord, prev_coord, next_coord, min_corner_angle_degree: float) -> bool:
+        prev_vec = Vector2D.from_coordinates(prev_coord, cur_coord)
+        next_vec = Vector2D.from_coordinates(cur_coord, next_coord)
+        angle_in_degree = prev_vec.angle_to(next_vec)
+        return angle_in_degree >= min_corner_angle_degree
